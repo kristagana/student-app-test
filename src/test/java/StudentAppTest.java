8be41ebc7941de6cc6058f71e14e6b378f2d865d
kristagana <krista.gana@gmail.com>
2023-03-25 23:56:09 +0200

    [master] Add classroom progress 20.03.2023

--- src/test/java/StudentAppTest.java
+++ src/test/java/StudentAppTest.java
@@ -1,17 +1,47 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class StudentAppTest {
+    WebDriver driver;
+    private final String APP_URL = "http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/";
+
+    @BeforeMethod
+    public void initialize() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get(APP_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
 
     @Test
     public void openStudentApp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/");
-        driver.close();
-        driver.quit();
+        WebElement addStudentButton = driver.findElement(By.xpath("//div[@class='ant-table-title']//button"));
+        addStudentButton.click();
+
+        WebElement nameField = driver.findElement(By.id("name"));
+        nameField.sendKeys("Krista");
+
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys("krista.gana@gmail.com");
+
+        WebElement submitButton = driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button"));
+        submitButton.click();
+
+//        driver.findElement(By.xpath("//div[@class='ant-table-title']//button")).click();
+//        driver.findElement(By.id("name")).sendKeys("Krista");
+//        driver.findElement(By.id("email")).sendKeys("krista.gana@gmail.com");
+//        driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button")).click();
     }
 }
